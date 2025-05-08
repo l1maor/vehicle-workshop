bc17978a1cc88d7d882d4d920a439151f77a86eb
Ed <lim4or@gmail.com>
2025-05-08 14:48:37 -0400

    first commit, setting up project and initial deps

---
+++ src/main/java/com/l1maor/vehicleworkshop/VehicleworkshopApplication.java
@@ -0,0 +1,13 @@
+package com.l1maor.vehicleworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleworkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleworkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/l1maor/vehicleworkshop/VehicleworkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l1maor.vehicleworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleworkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
